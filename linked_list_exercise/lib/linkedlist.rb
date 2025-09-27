d4d462aca493e56c9f53ccee69e007b84b58e962
elifnurozkaya <elifnurozkaya4334@gmail.com>
2025-09-27 23:10:35 +0300

    initialize linkedlist files

---
+++ linked_list_exercise/lib/linkedlist.rb
@@ -0,0 +1,52 @@
+require_relative 'nodes.rb'
+
+class LinkedList
+  def append(value)
+    node = Node.new
+    node.value = value
+  end
+
+  def prepend(value)
+    
+  end
+
+  def size
+    
+  end
+
+  def head
+    
+  end
+
+  def tail
+    
+  end
+
+  def at(index)
+    
+  end
+
+  def pop
+    
+  end
+
+  def contains?(value)
+    
+  end
+
+  def find(value)
+    
+  end
+
+  def to_s
+    
+  end
+
+  def insert_at(value,index)
+    
+  end
+
+  def remove_at(index)
+    
+  end
+end
---
+++ linked_list_exercise/lib/nodes.rb
@@ -0,0 +1,7 @@
+class Node
+  attr_accessor :value, :next_mode
+  def initialize
+    @value = nil
+    @next_node = nil
+  end
+end
