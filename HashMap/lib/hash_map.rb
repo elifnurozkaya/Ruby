09d6d00713082f70bd9b8dee91b820719b99e15a
elifnurozkaya <elifnurozkaya4334@gmail.com>
2025-10-03 23:45:18 +0300

    add the rest of the methods

--- HashMap/lib/hash_map.rb
+++ HashMap/lib/hash_map.rb
@@ -1,11 +1,12 @@
 require_relative 'node.rb'
 class HashMap
-  attr_accessor :load_factor, :capacity
+  attr_accessor :load_factor, :capacity, :length
 
-  def initialize(capacity = 16)
+  def initialize
     @load_factor = 0.75
-    @capacity = capacity
-    @buckets = Array.new(self.capacity)
+    @capacity = 16
+    @buckets = Array.new(@capacity)
+    @length = 0
   end
 
   def hash(key)
@@ -22,25 +23,61 @@ class HashMap
 
     if @buckets[hash_val] == nil
             @buckets[hash_val] = Node.new(key,value)
+            @length += 1
     else
       current = @buckets[hash_val]
       while current.next_node !=nil
         if current.key == key
           current.value = value
+          self.check
           return
         end
         current = current.next_node
       end
       if current.key == key
           current.value = value
+          self.check
           return
       end
+      
       current.next_node = Node.new(key,value)
+      @length += 1
     end
+    self.check
+    
+  end
+
+  def check
+    if @length > (@capacity * @load_factor)
+      self.grow_capacity
+    end
+  end
+
+  def grow_capacity
+    backup = @buckets
+    @capacity *= 2
+    @buckets = Array.new(@capacity)
+    @length = 0
+    
+
+    backup.each do |bucket|
+      if bucket != nil
+        current = bucket
+        while current
+          self.set(current.key,current.value)
+          current = current.next_node
+        end
+      end
+    end
+    
   end
 
   def get(key)
     hash_value = self.hash(key)
+
+    if @buckets[hash_value] == nil
+      return nil
+    end
 
     if @buckets[hash_value].key == key
       return @buckets[hash_value].value
@@ -52,7 +89,130 @@ class HashMap
         end
         current = current.next_node
       end
-      return nil
     end
   end
+
+  def has?(key)
+    if get(key) != nil
+      return true
+    else
+      return false
+    end
+  end
+
+  def remove(key)
+    hash_val = hash(key)
+
+    prev = nil
+    current = @buckets[hash_val]
+
+    return nil if @buckets[hash_val].nil?
+
+    if current.key == key && current.next_node == nil
+      return_value = current.value
+      @buckets[hash_val] = nil
+      @length -= 1
+      return return_value
+    elsif current.key == key && current.next_node != nil
+      return_value = current.value
+      @buckets[hash_val] = current.next_node
+      return return_value 
+    elsif current.key != key && current.next_node != nil
+      prev = current
+      current = current.next_node
+      while current 
+        if current.key == key
+          return_value = current.value
+          prev.next_node = current.next_node
+          return return_value
+        end
+        prev = current
+        current = current.next_node
+      end
+      nil
+    end
+  end
+
+def length
+  i = 0
+  current = @buckets[i]
+  count = 0
+
+  while i < @capacity
+    if current == nil
+      i += 1
+     current = @buckets[i]
+      next
+    end
+    count += 1
+    while current.next_node
+      count += 1
+      current = current.next_node
+    end
+    i += 1
+    current = @buckets[i]
+  end
+  return count
+end
+
+def clear(capacity)
+  self.initialize(capacity)
+end
+
+def keys
+  all_keys = []
+  current = @buckets[0]
+  i = 0
+  while i < @capacity
+    if current == nil
+      i += 1
+      current = @buckets[i]
+      next
+    end
+
+    all_keys.push(current.key)
+    while current
+      current = current.next_node
+      all_keys.push(current.key) if current != nil
+    end
+    i += 1
+    current = @buckets[i]
+  end
+  return all_keys
+end
+
+def values
+  all_values = []
+  current = @buckets[0]
+  i = 0
+  while i < @capacity
+    if current == nil
+      i += 1
+      current = @buckets[i]
+      next
+    end
+
+    all_values.push(current.value)
+    while current
+      current = current.next_node
+      all_values.push(current.value) if current != nil
+    end
+    i += 1
+    current = @buckets[i]
+  end
+  return all_values
+end
+
+def entries
+  keys = self.keys
+  values = self.values
+
+  entries = []
+  i = 0
+  while i < keys.length
+    entries.push("[#{keys[i]},#{values[i]}]")
+    i += 1
+  end
+  return entries
+end
 end
--- HashMap/main.rb
+++ HashMap/main.rb
@@ -15,6 +15,30 @@ test = HashMap.new
  test.set('kite', 'pink')
  test.set('lion', 'golden')
 
- puts test.get('apple')
- test.set('apple','healthy')
- puts test.get('apple')
+#  puts test.get('apple')
+#  test.set('apple','healthy')
+#  puts test.get('apple')
+
+#  puts test.has?("apple")
+#  puts test.has?("floor")
+
+#  puts test.get("apple")
+#  puts test.remove("apple")
+#  puts test.get("apple")
+
+#  puts test.length
+
+# test.clear(16)
+# puts test.length
+
+# puts test.keys
+
+# puts test.values
+
+# puts test.entries
+
+puts test.capacity
+puts test.length
+test.set('moon', 'silver')
+puts test.capacity
+puts test.length
